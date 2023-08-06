5f6eb41322ea4a09d8da200a9f488fcb323a1e23
Pragyansh Chaturvedi <pragyanshchaturvedi18@gmail.com>
2023-08-06 22:53:04 +0530

    init

---
+++ 2DClothSim.cpp
@@ -0,0 +1,12 @@
+﻿// 2DClothSim.cpp : Defines the entry point for the application.
+//
+
+#include "2DClothSim.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ 2DClothSim.h
@@ -0,0 +1,8 @@
+﻿// 2DClothSim.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
